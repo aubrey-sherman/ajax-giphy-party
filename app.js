66d7d2f4b9f04322872fd6a5694f5c90a82c6cbe
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-16 19:45:34 -0400

    Added seach form in HTML; added event listener on form for search submission.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+const $form = document.querySelector("#search-form");
+$form.addEventListener("submit", cb);
+
+// capture form search value in fetch request
