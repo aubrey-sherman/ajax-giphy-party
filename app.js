852db7cd77be5fa50ca8ab58568ab219e4413cf9
Andrea Jao <ndrjao@gmail.com>
2024-04-16 18:18:40 -0700

    finished exercise with review notes

--- app.js
+++ app.js
@@ -2,51 +2,55 @@
 
 console.log("Let's get this party started!");
 
-
-
-// capture form search value in fetch request
-// upon submit, call fetch
-//// > have to pass in input box target input / form input input area. value
-//// > fetch request with API = URL
-//// > request body method GET, body query, headers default
-//// > await response from fetch, assign to a variable (JSON, array of objects)
-//// > parsing with .json() >> console.log
-//// find the image for each element object
-
 const $gifContainer = document.querySelector("#gif-container");
 
-/** TODO: Write docstring */
-async function getGifs() {
-  document.pre;
-  const searchKeyWord = document.querySelector("#form-search-input").value;
+/** Given a gif array and an index, will append an image to the DOM
+ * and set the img src.
+ */
+function showGifs(singleGifData, index) { // update to reflect array , update name to showGif
 
-  const response = await fetch(
-    `http://api.giphy.com/v1/gifs/search?q=${searchKeyWord}` +
-    `&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
-  ); // actual results JSON
-
-  const dataHolder = await response.json(); // give us our object
-
-  console.log("finished fetch request, returning: ", dataHolder);
-  // call the show function
-
-  // grab a GIF from the response data and append the GIF to the page
-  const gifURL = dataHolder.data[0].images.original.url;
-  // create div with img src with above variable
+  console.log("in showgifs", singleGifData[index]);
+  const gifURL = singleGifData.data[index].images.original.url;
   console.log(gifURL);
 
   const $gifImg = document.createElement("img");
   $gifImg.src = gifURL;
-
   $gifContainer.appendChild($gifImg);
-
 }
 
-// Ensure that if you submit the form multiple times, you’ll get multiple GIFs
+/** When called, will return an array of gif objects from the Giphy API
+ *  and generate a random index based on the length of the array
+*/
+async function getGifs() { // it also calls the show function, which it ISN'T doing
 
+  const searchKeyWord = document.querySelector("#form-search-input").value; // comment on line 29
+
+  const params = new URLSearchParams({
+    q: searchKeyWord,
+    api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  });
+
+  const response = await fetch(
+    `http://api.giphy.com/v1/gifs/search?${params}` // use URL search param to allow it to make it to a string
+  );
+
+  const dataHolder = await response.json();
+  console.log("finished fetch request, returning: ", dataHolder);
+
+  const gifIndex = Math.floor(Math.random() * (dataHolder.data.length));
+  console.log("this is our random index: ", gifIndex);
+
+  showGifs(dataHolder, gifIndex);
+
+}
 
 const $form = document.querySelector("#search-form");
 $form.addEventListener("submit", function (event) {
   event.preventDefault();
   getGifs();
-}); // calling fetch
+});
+
+const $deleteBtn = document.querySelector("#delete-btn");
+$deleteBtn.addEventListener("click", function (event) {
+  $gifContainer.innerHTML = "";
+});;;
